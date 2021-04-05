bc1216c5d3c95aa51df3f7b2a0483c3948cb7d56
gigi <guka.tgs@gmail.com>
2021-04-04 20:36:04 -0400

    final

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,36 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+//function generateMarkdown(data) {
+  //return 
+  
+  `## Table of Contents:
+  *[Description](#Description)
+  *[Usage](#Usage)
+  *[Test](#Test)
+  *[License](#License)
+  *[GitHub](#GitHub)
+  *[Email](#Email)
 
-`;
-}
+  #Title:
+  # ${answers.title}
+
+  ## Description:
+  ${answers.description}
+
+  ## Usage:
+  ${answers.usage}
+
+  ## Test:
+  ${answers.test}
+
+  ## License:
+  ${answers.license}
+
+  ## GitHub
+  Please visit my ${answers.github} to see my work.
+
+  ## Email
+  My Email is ${answers.email}`
+
 
 module.exports = generateMarkdown;
